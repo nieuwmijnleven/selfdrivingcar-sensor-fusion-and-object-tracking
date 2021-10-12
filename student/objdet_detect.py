f9c47452bce8dc4e1f674dab80e5776f5d6fe673
real.overcomer.jc@gmail.com <real.overcomer.jc@gmail.com>
2021-10-12 10:10:51 +0200

    solved S3 problems

--- student/objdet_detect.py
+++ student/objdet_detect.py
@@ -24,7 +24,7 @@ sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 
 # model-related
 from tools.objdet_models.resnet.models import fpn_resnet
-from tools.objdet_models.resnet.utils.evaluation_utils import decode, post_processing 
+from tools.objdet_models.resnet.utils.evaluation_utils import decode, post_processing
 
 from tools.objdet_models.darknet.models.darknet2pytorch import Darknet as darknet
 from tools.objdet_models.darknet.utils.evaluation_utils import post_processing_v2
@@ -61,6 +61,51 @@ def load_configs_model(model_name='darknet', configs=None):
         ####### ID_S3_EX1-3 START #######     
         #######
         print("student task ID_S3_EX1-3")
+        configs.model_path = os.path.join(parent_path, 'tools', 'objdet_models', 'resnet')
+        configs.pretrained_filename = os.path.join(configs.model_path, 'pretrained', 'fpn_resnet_18_epoch_300.pth')
+        configs.arch = 'fpn_resnet'
+        # configs.batch_size = 4
+        # configs.conf_thresh = 0.5
+        # configs.distributed = False # For testing on 1 GPU only
+        # configs.img_size = 608
+        # configs.nms_thresh = 0.4
+        # configs.num_samples = None
+        # configs.num_workers = 4
+        # configs.pin_memory = True
+        # configs.use_giou_loss = False
+
+        configs.K = 50
+        configs.num_samples = None
+        configs.num_workers = 4
+        configs.batch_size = 4
+        #configs.peak_thresh = 0.2
+        configs.conf_thresh = 0.5
+
+        configs.pin_memory = True
+        configs.distributed = False # For testing on 1 GPU only
+
+        configs.input_size = (608, 608)
+        configs.hm_size = (152, 152)
+        configs.down_ratio = 4
+        configs.max_objects = 50
+
+        configs.num_layers = 18
+        configs.imagenet_pretrained = False
+        configs.head_conv = 64
+        configs.num_classes = 3
+        configs.num_center_offset = 2
+        configs.num_z = 1
+        configs.num_dim = 3
+        configs.num_direction = 2  # sin, cos
+
+        configs.heads = {
+            'hm_cen': configs.num_classes,
+            'cen_offset': configs.num_center_offset,
+            'direction': configs.num_direction,
+            'z_coor': configs.num_z,
+            'dim': configs.num_dim
+        }
+        configs.num_input_features = 4
 
         #######
         ####### ID_S3_EX1-3 END #######     
@@ -118,7 +163,7 @@ def create_model(configs):
         ####### ID_S3_EX1-4 START #######     
         #######
         print("student task ID_S3_EX1-4")
-
+        model = fpn_resnet.get_pose_net(num_layers = configs.num_layers, heads = configs.heads, head_conv = configs.head_conv, imagenet_pretrained = configs.imagenet_pretrained)
         #######
         ####### ID_S3_EX1-4 END #######     
     
@@ -167,7 +212,12 @@ def detect_objects(input_bev_maps, model, configs):
             ####### ID_S3_EX1-5 START #######     
             #######
             print("student task ID_S3_EX1-5")
-
+            detections = decode(outputs['hm_cen'], outputs['cen_offset'], outputs['direction'], outputs['z_coor'],
+                                outputs['dim'], K=configs.K)
+            detections = detections.cpu().numpy().astype(np.float32)
+            #detections = post_processing(detections, configs.num_classes, configs.down_ratio, configs.peak_thresh)
+            detections = post_processing(detections, configs)
+            detections = detections[0]  # only first batch
             #######
             ####### ID_S3_EX1-5 END #######     
 
@@ -179,13 +229,15 @@ def detect_objects(input_bev_maps, model, configs):
     print("student task ID_S3_EX2")
     objects = [] 
 
+    VEHICLE_LABEL = 1
     ## step 1 : check whether there are any detections
-
+    if len(detections[VEHICLE_LABEL]) > 0:
         ## step 2 : loop over all detections
-        
+        for det in detections[VEHICLE_LABEL]:
             ## step 3 : perform the conversion using the limits for x, y and z set in the configs structure
-        
+            _, x, y, z, h, w, l, yaw = det
             ## step 4 : append the current object to the 'objects' array
+            objects.append([VEHICLE_LABEL, x, y, z, h, w, l, yaw])
         
     #######
     ####### ID_S3_EX2 START #######   
